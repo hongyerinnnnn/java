c31558e3b80ba31cb9c2f1859551bf09c7ba144b
Hong Ye Rin <yerinnn526@naver.com>
2025-08-09 10:55:46 +0900

    first commit

--- object/collection/MapEx1.java
+++ object/collection/MapEx1.java
@@ -1,7 +1,9 @@
 package com.multi.object.collection;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class MapEx1 {
     public void mapMethodEx1() {
@@ -9,8 +11,23 @@ public class MapEx1 {
         hashMap.put(1, "one");
         hashMap.put(2, "two");
         hashMap.put(3, "three");
-        System.out.println(hashMap);//toString호출
-        hashMap.get(1).startsWith("o");
+
+        Set<Integer> set = hashMap.keySet();
+        Iterator<Integer> iterator = set.iterator();
+//        while (iterator.hasNext()) {
+//            int key = iterator.next();
+//            System.out.println(key + ":" + hashMap.get(key));
+//        }
+
+        Iterator<Map.Entry<Integer, String>> iter = hashMap.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<Integer, String> entry = iter.next();
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+        //        System.out.println(hashMap);//toString호출
+
+//        hashMap.get(1).startsWith("o");
     }
 
     public static void main(String[] args) {
